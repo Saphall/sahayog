eb4263195429bca204cf433ba6f504325a3ed44b
Sunam Shrestha <sunam.shrestha@f1soft.com>
2021-02-19 19:39:50 +0545

    index and login page added

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="">
+		<p>
+			<label for="username">Username</label>
+			<input type="username" name="username">
+		</p>
+		<p>
+			<label for="password">Password</label>
+			<input type="password" name="password">
+		</p>
+
+		<input type="submit" name="btn_submit">
+	</form>
+	
+</body>
+</html>
