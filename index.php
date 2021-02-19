02525536ab04ed5e3218b4fd5c2d4d69fe2e6597
Santosh Bhandari <contact@santoshb.com.np>
2021-02-19 19:53:41 +0545

    Update home page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sahayog - Helping people find their lost ones!</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Footer -->
+    <div class="flex flex-wrap justify-between gap-y-5 p-2 bg-gray-800 text-gray-100">
+        <div class="font-semibold text-xl">Sahayog</div>
+        <div class="flex flex-wrap gap-x-5">
+            <a href="#">Home</a>
+            <a href="#">List</a>
+            <a href="#">Report</a>
+        </div>
+    </div>
+    <!-- Footer ends here -->
+    <div class="text-center mt-10 px-5">
+        <p class="text-6xl">Sahayog</p>
+        <p class="mt-5 text-xl text-gray-700 italic">Helping people find their lost ones!</p>
+    </div>
+</body>
+</html>
