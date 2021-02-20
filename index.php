b1d4303e256099ba0a12345f927c8db93f01db89
Saphall <safalsakha@gmail.com>
2021-02-20 10:25:24 +0545

    responsive-custom-navigation-configured

--- index.php
+++ index.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Sahayog - Helping people find their lost ones!</title>
+    <title>Sahayog - Helping people find their lost ones!</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <script src="https://kit.fontawesome.com/fb9d3931c2.js" crossorigin="anonymous"></script>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <!-- Footer -->
-    <div class="flex flex-wrap justify-between gap-y-5 p-2 bg-gray-800 text-gray-100">
-        <div class="font-semibold text-xl">Sahayog</div>
-        <div class="flex flex-wrap gap-x-5">
-            <a href="#">Home</a>
-            <a href="#">List</a>
-            <a href="#">Report</a>
+    <!-- NavBar -->
+    <nav class="navbar">
+        <div class="navbar__container">
+            <a href="#" id="navbar__logo"><i class="fas fa-praying-hands"></i> SAHAYOG</a>
+
+            <div class="navbar__toogle" id="mobile-menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+
+            <ul class="navbar__menu">
+                <li class="navbar__item"><a href="#" class="navbar__links">Home</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__links">List</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__links">Report</a> </li>
+                <li class="navbar__btn"><a href="#" class="button">Sign Up</a> </li>
+            </ul>
         </div>
-    </div>
-    <!-- Footer ends here -->
+    </nav>
+
+    <!--content-->
+
     <div class="text-center mt-10 px-5">
         <p class="text-6xl">Sahayog</p>
         <p class="mt-5 text-xl text-gray-700 italic">Helping people find their lost ones!</p>
     </div>
+
+
+    <script src="JS/app.js"></script>
 </body>
+
 </html>
