6818785c9f8312e6e4f6dd7461941571b97b394f
Santosh Bhandari <contact@santoshb.com.np>
2021-02-20 16:41:20 +0545

    Fixes

--- user/post.php
+++ user/post.php
@@ -23,51 +23,52 @@
             </div>
             <div class="mt-5">
                 <div class="flex flex-col gap-y-2">
-                <div class="flex flex-wrap gap-3">
-                    <!-- Name -->
-                    <div align="start" class="w-full md:w-64">
-                        <label for="fullname">Full Name</label>
-                        <input id="fullname" name="fullname" type="text" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Name" />
-                    </div>
-                    <!-- Age -->
-                    <div align="start" class="w-full md:w-64">
-                        <label for="age">Age</label>
-                        <input id="age" name="age" type="number" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Age" />
-                    </div>
-                </div>
-                <!-- Gender -->
-                <div align="start" class="my-3">
-                    <label for="">Gender</label>
-                    <div class="flex flex-wrap gap-x-5 items-center">
-                        <div class="flex items-center">
-                            <input id="male" name="gender" type="radio" value="male" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                            <label for="male" class="ml-1 block text-sm font-medium text-gray-700"> Male </label>
+                    <div class="flex flex-wrap gap-3">
+                        <!-- Name -->
+                        <div align="start" class="w-full md:w-64">
+                            <label for="fullname">Full Name</label>
+                            <input id="fullname" name="fullname" type="text" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Name" />
                         </div>
-                        <div class="flex items-center">
-                            <input id="female" name="gender" type="radio" value="female" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                            <label for="female" class="ml-1 block text-sm font-medium text-gray-700"> Female </label>
-                        </div>
-                        <div class="flex items-center">
-                            <input id="other" name="gender" type="radio" value="other" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
-                            <label for="other" class="ml-1 block text-sm font-medium text-gray-700"> Other </label>
+                        <!-- Age -->
+                        <div align="start" class="w-full md:w-64">
+                            <label for="age">Age</label>
+                            <input id="age" name="age" type="number" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Age" />
                         </div>
                     </div>
-                </div>
-                <!-- Address -->
-                <div align="start" class="w-full md:w-64">
-                    <label for="address">Address</label>
-                    <input id="address" name="address" type="text" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Address" />
-                </div>
-                <div class="flex flex-wrap gap-3">
-                    <!-- Lost on -->
-                    <div align="start" class="w-full md:w-64">
-                        <label for="lost_on">Lost on</label>
-                        <input id="lost_on" name="lost_on" type="date" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+                    <!-- Gender -->
+                    <div align="start" class="my-3">
+                        <label for="">Gender</label>
+                        <div class="flex flex-wrap gap-x-5 items-center">
+                            <div class="flex items-center">
+                                <input id="male" name="gender" type="radio" value="male" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                                <label for="male" class="ml-1 block text-sm font-medium text-gray-700"> Male </label>
+                            </div>
+                            <div class="flex items-center">
+                                <input id="female" name="gender" type="radio" value="female" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                                <label for="female" class="ml-1 block text-sm font-medium text-gray-700"> Female </label>
+                            </div>
+                            <div class="flex items-center">
+                                <input id="other" name="gender" type="radio" value="other" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                                <label for="other" class="ml-1 block text-sm font-medium text-gray-700"> Other </label>
+                            </div>
+                        </div>
                     </div>
-                    <!-- Lost from -->
+                    <!-- Address -->
                     <div align="start" class="w-full md:w-64">
-                        <label for="lost_from">Lost from</label>
-                        <input id="lost_from" name="lost_from" type="text" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Last known location" />
+                        <label for="address">Address</label>
+                        <input id="address" name="address" type="text" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Address" />
+                    </div>
+                    <div class="flex flex-wrap gap-3">
+                        <!-- Lost on -->
+                        <div align="start" class="w-full md:w-64">
+                            <label for="lost_on">Lost on</label>
+                            <input id="lost_on" name="lost_on" type="date" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+                        </div>
+                        <!-- Lost from -->
+                        <div align="start" class="w-full md:w-64">
+                            <label for="lost_from">Lost from</label>
+                            <input id="lost_from" name="lost_from" type="text" required class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Last known location" />
+                        </div>
                     </div>
                     <!-- Relation -->
                     <div align="start" class="w-full md:w-64">
@@ -78,7 +79,6 @@
                         <label for="description">Other Details</label>
                         <textarea id="description" rows="5" name="description" class="appearance-none rounded-none relative block w-full md:w-64 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" /></textarea>
                     </div>
-                </div>
                 </div>
             </div>
             <div class="text-center">
