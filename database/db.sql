914c776650503b50eb0f414ed43a96d9fb7b4241
Sunam Shrestha <sunam.shrestha@f1soft.com>
2021-02-20 10:12:49 +0545

    db file added

---
+++ database/db.sql
@@ -0,0 +1,69 @@
+/*
+SQLyog Professional v13.1.1 (32 bit)
+MySQL - 10.4.11-MariaDB : Database - sahayog
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sahayog` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `sahayog`;
+
+/*Table structure for table `details` */
+
+DROP TABLE IF EXISTS `details`;
+
+CREATE TABLE `details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `address1` varchar(255) NOT NULL,
+  `address2` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `details` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 0,
+  `token` varchar(255) DEFAULT NULL,
+  `type` varchar(255) NOT NULL,
+  `active` tinyint(1) DEFAULT 1,
+  `deleted` tinyint(1) DEFAULT 0,
+  `detail` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `detail` (`detail`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`detail`) REFERENCES `details` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`email`,`password`,`verified`,`token`,`type`,`active`,`deleted`,`detail`) values 
+(1,'superadmin','superadmin@mail.com','$2y$10$nE1zPUtd0eEggGunK3gK5uQJYahG9qASWap6rLCmMi7dnSsyXSpam',1,'5509124336cf4f2f0bc82a544972717f19070a19131e4e7797ceaf1b28615ab22ddde47f427013a53608439492b391e2ce76','superadmin',1,0,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
