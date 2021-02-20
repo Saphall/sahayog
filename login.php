595dc605cf10132676068e46a5b83817eba7c294
Saphall <safalsakha@gmail.com>
2021-02-20 16:19:35 +0545

    login-little-modify

--- login.php
+++ login.php
@@ -1,44 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title>Sahayog - Helping people find their lost ones!</title>
+    <title>Sahayog - Helping people find their lost ones!</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/fb9d3931c2.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Kumbh Sans', sans-serif;
+        }
+
+        #title {
+            background-color: #ff8177;
+            background-image: linear-gradient(to top, #ff8855 0%, #ff0844 0%, #ac6049 100%);
+            background-size: 100%;
+            -webkit-background-clip: text;
+            -moz-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            -moz-text-fill-color: transparent;
+            align-items: center;
+            cursor: pointer;
+            text-decoration: none;
+            font-size: 2rem;
+        }
+
+        #login {
+            background-color: rgb(46, 46, 197);
+        }
+    </style>
 </head>
+
 <body>
-	<div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-  		<div class="max-w-md w-full space-y-8">
-    		<div>
-		      	<h1 class="text-5xl text-center font-bold uppercase">Sahayog</h1>
-      			<h2 class="mt-6 text-center text-2xl font-semibold text-gray-600">Sign in to your account</h2>
-    		</div>
+    <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                <h1 class="text-5xl text-center font-bold uppercase" id='title'><i class="fas fa-praying-hands"></i> SAHAYOG</h1>
+                <h2 class="mt-6 text-center text-2xl font-semibold text-gray-600">Sign in to your account</h2>
+            </div>
 
-    		<form class="mt-8 space-y-6" action="login_action" method="POST">
-      			<input type="hidden" name="remember" value="true" />
-      			<div class="rounded-md shadow-sm -space-y-px">
-        			<div>
-			        	<label for="email-address" class="sr-only">Email address</label>
-          				<input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
-        			</div>
-        			<div>
-          				<label for="password" class="sr-only">Password</label>
-          				<input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
-        			</div>
-      			</div>
+            <form class="mt-8 space-y-6" action="login_action.php" method="POST">
+                <input type="hidden" name="remember" value="true" />
+                <div class="rounded-md shadow-sm -space-y-px">
+                    <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+                    </div>
+                </div>
 
-      			<div>
-        			<button type="submit" name="btn_login" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-          				<span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            				<svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              					<path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-            				</svg>
-          				</span>
-          				<span>Sign in</span>
-        			</button>
-      			</div>
-    		</form>
-  		</div>
-	</div>
+                <div>
+                    <button id="login" type="submit" name="btn_login" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        <span>Sign in</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        document.getElementById("title").onclick = function() {
+            window.location.href = ("#");
+        }
+    </script>
 </body>
+
 </html>
