283f88ab183582587812685b4ba0e2717497f32c
Saphall <safalsakha@gmail.com>
2021-02-20 13:52:09 +0545

    homepage design

--- index.php
+++ index.php
@@ -13,12 +13,27 @@
 <body>
     <!-- NavBar -->
     <?php require 'includes/navbar.php'; ?>
+<!--content-->
+    <div class="main">
+        <div class="main__container">
+            <div class="main__content">
+                <h1>SAHAYOG</h1>
+                <p>Helping people find their lost ones!</p>
+            </div>
+            <div class="main__img--container">
+                <img src="Images/Logo1.png" alt="" id="main__img">
+            </div>
+        </div>
+    </div>
 
-    <!--content-->
-
-    <div class="text-center mt-10 px-5">
+    <!-- <div class="text-center mt-10 px-5">
         <p class="text-6xl">Sahayog</p>
         <p class="mt-5 text-xl text-gray-700 italic">Helping people find their lost ones!</p>
+    </div> -->
+
+    <!--latestReports-->
+    <div class="report">
+        <p> This contains images of latest lost.</p>
     </div>
 
 
